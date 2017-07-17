50e0fa62fb6af06eaff37c669bbafd9b6a71219d
Breyana Scales <breyanaj@gmail.com>
2017-07-17 10:47:48 -0700

    Restructure functions to support multiple functions, change context in month test

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,4 +1,4 @@
-module.exports = (date) => {
+const month = (date) => {
   if (typeof date !== "object") {
     throw new Error("Please pass in a Date object")
   }
@@ -11,4 +11,8 @@ module.exports = (date) => {
   ]
   let month = date.getMonth()
   return months[month]
+}
+
+module.exports = {
+  month
 }
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,8 +1,8 @@
-const month = require('./functions')
+const { month } = require('./functions')
 const { expect } = require('chai')
 
 describe('Month Function', () => {
-  context('Passing Tests', () => {
+  context('Expected Behavior', () => {
     it('Returns the month of a given Date', () => {
       let date = new Date(2017, 5, 19)
       expect(month(date)).to.equal("June")
